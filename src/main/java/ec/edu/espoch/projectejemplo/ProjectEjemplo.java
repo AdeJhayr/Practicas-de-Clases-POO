2d31d19d2117256c218bbcb8b1ac155d356636d8
AdeJhayr <brandonjaramillo763>
2025-09-30 21:07:58 -0500

    Creacion de la clase de estudiante y curso

---
+++ src/main/java/ec/edu/espoch/projectejemplo/ProjectEjemplo.java
@@ -0,0 +1,29 @@
+package ec.edu.espoch.projectejemplo;
+
+import java.util.Scanner;
+
+public class ProjectEjemplo 
+{
+    public static void main(String[] args) 
+    {
+        Scanner entrada = new Scanner(System.in);
+        
+        System.out.println("Ingrese la operacion que desea hacer");
+        System.out.println("1. Suma");
+        System.out.println("2. resta");
+        int var =0;
+        switch (var) 
+        {
+            case 1:
+                
+                break;
+            case 2:
+                
+                break;
+            default:
+                throw new AssertionError();
+        }
+
+    }
+
+}
